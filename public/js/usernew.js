cf0ffbab659f5840894b9c3f7f042a44118462fb
Fernando Jiménez <ferjmoreno@gmail.com>
2013-01-29 13:26:33 +0100

    Feedback, login and new user views tweaks

--- public/js/usernew.js
+++ public/js/usernew.js
@@ -1,8 +1,19 @@
 function showSIMDetails(selectObj) {
   var phoneNumber = document.getElementById('phone_number');
+  var carrier = document.getElementById('carrier');
   if (selectObj.options[selectObj.selectedIndex].value == 1) {
     phoneNumber.style.display = 'block';
+    carrier.value = 'Movistar';
   } else {
     phoneNumber.style.display = 'none';
+  }
+}
+
+function showOtherCarrier(selectObj) {
+  var otherCarrier = document.getElementById('other_carrier');
+  if (selectObj.options[selectObj.selectedIndex].value == 'Other') {
+    otherCarrier.style.display = 'block';
+  } else {
+    otherCarrier.style.display = 'none';
   }
 }

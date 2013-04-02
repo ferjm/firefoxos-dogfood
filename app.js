e4dc26ee5ff268cd0a40861bdfacaa86f076062d
Fernando Jiménez <ferjmoreno@gmail.com>
2013-04-02 14:39:41 +0200

    Login/Admin menu

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ passport.use(new LocalStrategy(function(username, password, done) {
 }));
 
 // Home
-app.get('/', feedback.getAll);
+app.get('/', feedback.form);
 
 // Feedback
 app.post('/', feedback.deviceFormHandler);

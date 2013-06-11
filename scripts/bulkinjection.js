b12870808016e97a21857638d4599a8f4d3c1188
mariopl <mpl_yo@hotmail.com>
2013-06-11 13:45:05 +0200

    bulkinjection fixed

--- scripts/bulkinjection.js
+++ scripts/bulkinjection.js
@@ -1,4 +1,4 @@
-var config = require('./config.js'),
+var config = require('../config.js'),
     mongoose = require('mongoose'),
     parseXlsx = require('excel'),
     apiUser = require('./models/apiUser.js');
